da8b6b6d97f5864e86a21fea0460a970b7882881
Chris <62375193+ChrisLad1@users.noreply.github.com>
2021-10-20 23:37:48 +1100

    Revert "Added chromatic aberration"
    This reverts commit da209feba111f4491caca24e0db20482817f2b6f.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,5 @@
 package;
 
-import openfl.filters.BitmapFilterShader;
 #if desktop
 import Discord.DiscordClient;
 #end
@@ -43,18 +42,14 @@ import lime.utils.Assets;
 import openfl.display.BlendMode;
 import openfl.display.StageQuality;
 import openfl.filters.ShaderFilter;
-import openFl.utils.*;
-import openfl.filters.BitmapFilter;
 import openfl.utils.Assets as OpenFlAssets;
 import editors.ChartingState;
 import editors.CharacterEditorState;
 import flixel.group.FlxSpriteGroup;
-import ShadersHandler;
 import Achievements;
 import StageData;
 import FunkinLua;
 import DialogueBoxPsych;
-import RadialBlur;
 import Note;
 
 
@@ -66,9 +61,6 @@ using StringTools;
 
 class PlayState extends MusicBeatState
 {
-
-	var filters:Array<BitmapFilter> = [];
-
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
@@ -262,10 +254,6 @@ class PlayState extends MusicBeatState
 	// Lua shit
 	private var luaDebugGroup:FlxTypedGroup<DebugLuaText>;
 	public var introSoundsSuffix:String = '';
-	
-	
-	var ch = 2 / 1000;
-	var shadersLoaded:Bool = false;
 
 	override public function create()
 	{
@@ -289,13 +277,6 @@ class PlayState extends MusicBeatState
 		FlxG.cameras.add(camHUD);
 		FlxG.cameras.add(camOther);
 		grpNoteSplashes = new FlxTypedGroup<NoteSplash>();
-
-		camGame.setFilters(filters);
-		camGame.filtersEnabled = true;
-		camHUD.setFilters(filters);
-		camHUD.filtersEnabled = true;
-		camOther.filtersEnabled = true;
-		camOther.setFilters(filters);
 
 		#if desktop
 		FlxCamera.defaultCameras = [camGame];
@@ -1718,11 +1699,6 @@ class PlayState extends MusicBeatState
 		super.onFocusLost();
 	}
 
-
-	
-		
-
-
 	function resyncVocals():Void
 	{
 		if(finishTimer != null) return;
@@ -1739,8 +1715,6 @@ class PlayState extends MusicBeatState
 	var startedCountdown:Bool = false;
 	var canPause:Bool = true;
 	var limoSpeed:Float = 0;
-    
-	
 
 	override public function update(elapsed:Float)
 	{
@@ -1748,14 +1722,7 @@ class PlayState extends MusicBeatState
 		{
 			iconP1.swapOldIcon();
 		}*/
-		
-		if (!endingSong && !shadersLoaded)
-		{
-			shadersLoaded = true;
-			filters.push(ShadersHandler.chromaticAberration);
-			filters.push(ShadersHandler.radialBlur);
-		}
-		
+
 		callOnLuas('onUpdate', [elapsed]);
 
 		switch (curStage)
@@ -1884,12 +1851,6 @@ class PlayState extends MusicBeatState
 		}
 
 		super.update(elapsed);
-
-
-		ch = FlxG.random.int(1,5) / 900;
-		ch = FlxG.random.int(1,5) / 1000;
-		ShadersHandler.setChrome(ch);
-		ShadersHandler.setRadialBlur(640+(FlxG.random.int(-10,10)),360+(FlxG.random.int(-10,10)),FlxG.random.float(0.001,0.005));
 
 		if(ratingString == '?') {
 			scoreTxt.text = 'Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingString;
@@ -3803,9 +3764,6 @@ class PlayState extends MusicBeatState
 	override function beatHit()
 	{
 		super.beatHit();
-
-
-		
 
 		if(lastBeatHit >= curBeat) {
 			trace('BEAT HIT: ' + curBeat + ', LAST HIT: ' + lastBeatHit);
